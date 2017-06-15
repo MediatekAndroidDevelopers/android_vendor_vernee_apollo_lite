fd81a32e12aa6b53ed3a1da11922fef96cbf81db
fire855 <thefire855@gmail.com>
2017-06-16 00:38:24 +0200

    Update to N1 blobs

--- apollo_lite-vendor-blobs.mk
+++ apollo_lite-vendor-blobs.mk
@@ -1,13 +1,7 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/vernee/apollo_lite/usr,system/usr)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/vernee/apollo_lite/vendor,system/vendor)
+
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/vernee/apollo_lite/lib,system/lib)
 
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/vernee/apollo_lite/lib64,system/lib64)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/vernee/apollo_lite/bin,system/bin)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/vernee/apollo_lite/etc,system/etc)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/vernee/apollo_lite/usr,system/usr)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/vernee/apollo_lite/xbin,system/xbin)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/vernee/apollo_lite/vendor,system/vendor)
